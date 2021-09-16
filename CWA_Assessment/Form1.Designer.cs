1e6f2cd3054ac1bdf87a58eba4b7d4c98af69390
mxoh <mxohdapage@gmail.com>
2021-09-16 20:24:35 +0200

    master

--- CWA_Assessment/Form1.Designer.cs
+++ CWA_Assessment/Form1.Designer.cs
@@ -29,20 +29,134 @@ namespace CWA_Assessment
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.TransactionsBTN = new System.Windows.Forms.PictureBox();
+            this.trailBTN = new System.Windows.Forms.PictureBox();
+            this.closeBTN = new System.Windows.Forms.PictureBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TransactionsBTN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trailBTN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBTN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label1.Location = new System.Drawing.Point(513, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Transactions";
+            // 
+            // TransactionsBTN
+            // 
+            this.TransactionsBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.TransactionsBTN.Image = global::CWA_Assessment.Properties.Resources.transfer_icon;
+            this.TransactionsBTN.Location = new System.Drawing.Point(472, 224);
+            this.TransactionsBTN.Name = "TransactionsBTN";
+            this.TransactionsBTN.Size = new System.Drawing.Size(169, 94);
+            this.TransactionsBTN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TransactionsBTN.TabIndex = 3;
+            this.TransactionsBTN.TabStop = false;
+            this.TransactionsBTN.Click += new System.EventHandler(this.TransactionsBTN_Click);
+            this.TransactionsBTN.MouseLeave += new System.EventHandler(this.TransactionsBTN_MouseLeave);
+            this.TransactionsBTN.MouseHover += new System.EventHandler(this.TransactionsBTN_MouseHover);
+            // 
+            // trailBTN
+            // 
+            this.trailBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trailBTN.Image = global::CWA_Assessment.Properties.Resources.trail;
+            this.trailBTN.Location = new System.Drawing.Point(165, 224);
+            this.trailBTN.Name = "trailBTN";
+            this.trailBTN.Size = new System.Drawing.Size(169, 94);
+            this.trailBTN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trailBTN.TabIndex = 2;
+            this.trailBTN.TabStop = false;
+            this.trailBTN.Click += new System.EventHandler(this.trailBTN_Click);
+            this.trailBTN.MouseLeave += new System.EventHandler(this.trailBTN_MouseLeave);
+            this.trailBTN.MouseHover += new System.EventHandler(this.trailBTN_MouseHover);
+            // 
+            // closeBTN
+            // 
+            this.closeBTN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeBTN.Image = ((System.Drawing.Image)(resources.GetObject("closeBTN.Image")));
+            this.closeBTN.Location = new System.Drawing.Point(732, 12);
+            this.closeBTN.Name = "closeBTN";
+            this.closeBTN.Size = new System.Drawing.Size(38, 28);
+            this.closeBTN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.closeBTN.TabIndex = 1;
+            this.closeBTN.TabStop = false;
+            this.closeBTN.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.closeBTN.MouseLeave += new System.EventHandler(this.closeBTN_MouseLeave);
+            this.closeBTN.MouseHover += new System.EventHandler(this.closeBTN_MouseHover);
+            // 
+            // logo
+            // 
+            this.logo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logo.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.logo.Image = global::CWA_Assessment.Properties.Resources.logo;
+            this.logo.Location = new System.Drawing.Point(12, 12);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(140, 60);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo.TabIndex = 0;
+            this.logo.TabStop = false;
+            this.logo.Click += new System.EventHandler(this.logo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft PhagsPa", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label2.Location = new System.Drawing.Point(200, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Trail Balance";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TransactionsBTN);
+            this.Controls.Add(this.trailBTN);
+            this.Controls.Add(this.closeBTN);
+            this.Controls.Add(this.logo);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.TransactionsBTN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trailBTN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBTN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.PictureBox closeBTN;
+        private System.Windows.Forms.PictureBox trailBTN;
+        private System.Windows.Forms.PictureBox TransactionsBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CWA_Assessment/Form1.cs
+++ CWA_Assessment/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using PervasiveImporter__32_bit_;
+using Npgsql;
 
 namespace CWA_Assessment
 {
@@ -17,5 +21,120 @@ namespace CWA_Assessment
             InitializeComponent();
         }
 
+        /*https://www.youtube.com/watch?v=OoIk4ahkDmo */
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will close down the whole application. Confirm?", "Close Application", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                MessageBox.Show("The application has been closed successfully.", "Application Closed!", MessageBoxButtons.OK);
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                this.Activate();
+            }
+        }
+
+        private void trailBTN_Click(object sender, EventArgs e)
+        {
+            //32 bit is used to create access to the  PervasiveImporter__32_bit_
+
+            PSQLImport32 PSQL32 = new PSQLImport32();
+
+            //string dataLoc = "C:\\Development\\Accounting\\Pastel 8\\Accounting\\Source\\_Demo\\Documents\\Labels\\CustomerSupplier";
+            string dataLoc = "C:\\Development\\Accounting\\Pastel 8\\Accounting\\Source\\_Demo";
+            string server = "Server = \"\"";
+            string query1 = "SELECT * FROM";
+            string query2 = "SELECT * FROM C:\\Development\\Accounting\\Pastel 8\\Accounting\\Source\\_Demo\\Documents\\Labels\\CustomerSupplier\\Fields.pmd";
+
+            try
+            {
+                object result = PSQL32.sync_ImportSQL(dataLoc, server, query2);
+                
+                if (result.ToString().Length > 0)
+                {
+                    /*TextWriter writer = new StreamWriter(@"C:\folder\Text.txt");
+
+                    for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                        {
+                            writer.Write("\t" + dataGridView1.Rows[i].Cells[j].Value.ToString() + "\t" + "|");
+                        }
+                        writer.WriteLine("");
+                    }
+                    writer.Close();
+                    MessageBox.Show("Data Exported");*/
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+
+            /**
+             * Within this variable you can access Patel data in to forms
+             * Quering a table or running a SQL qury against multiple tables
+             * example of doing the connection :
+             * PSQL32.sync_ImportSQL(Path to the Pastel data , Server this must stay as : 'Server = ""', Table name or SQL query)
+             */
+
+        }
+
+        private void TransactionsBTN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TransactionsBTN_MouseHover(object sender, EventArgs e)
+        {
+            TransactionsBTN.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void TransactionsBTN_MouseLeave(object sender, EventArgs e)
+        {
+            TransactionsBTN.BorderStyle = BorderStyle.None;
+        }
+
+        private void trailBTN_MouseHover(object sender, EventArgs e)
+        {
+            trailBTN.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void trailBTN_MouseLeave(object sender, EventArgs e)
+        {
+            trailBTN.BorderStyle = BorderStyle.None;
+        }
+
+        private void closeBTN_MouseHover(object sender, EventArgs e)
+        {
+            closeBTN.BorderStyle = BorderStyle.FixedSingle;
+            closeBTN.BackColor = Color.Aqua;
+        }
+
+        private void closeBTN_MouseLeave(object sender, EventArgs e)
+        {
+            closeBTN.BorderStyle = BorderStyle.None;
+            closeBTN.BackColor = Color.White;
+        }
+
+        private void logo_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will open AdaptIT site down. Confirm?", "Close Application", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("https://www.adaptit.com/");
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                this.Activate();
+            }
+        }
     }
 }
